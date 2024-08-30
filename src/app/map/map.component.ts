a1d7682a92bb016e5fa1a34160b16cc77a9f98d5
Isma <ismamoyano97@gmail.com>
2024-08-30 15:21:15 +0200

    mapafunca

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,6 +4,18 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import { OSM } from 'ol/source';
 import TileLayer from 'ol/layer/Tile';
+import Static from "ol/source/ImageStatic";
+import ImageLayer from "ol/layer/Image";
+import {Projection} from "ol/proj";
+
+const extent = [0, 0, 3111, 2209];
+const projection = new Projection({
+  code: 'xkcd-image',
+  units: 'pixels',
+  extent: extent,
+});
+
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -15,14 +27,21 @@ export class MapComponent implements OnInit {
   ngOnInit(): void {
     this.map = new Map({
       layers: [
-        new TileLayer({
-          source: new OSM(),
+        new ImageLayer({
+          source: new Static({
+            url: 'map.png',
+            projection: projection,
+            imageExtent: extent,
+          }),
         }),
       ],
       target: 'map',
       view: new View({
-        center: [0, 0],
-        zoom: 2,maxZoom: 18,
+        projection: projection,
+        center: [1555, 1105],
+        zoom: 2,
+        maxZoom: 4,
+        minZoom: 2,
       }),
     });
   }
